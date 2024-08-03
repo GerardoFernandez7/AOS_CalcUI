754ff7a1122e21fc835bd866e92aa15e00186d20
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-08-02 20:36:05 -0600

    Se creo el proyecto

---
+++ app/src/main/java/com/gerardo_fdez/aosp_calcui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.gerardo_fdez.aosp_calcui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // Componentes
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main) // Botones
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
